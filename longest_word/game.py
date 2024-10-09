8719fcba647c853aaa05bb9dafd3e3a5e9f0244d
jeanmichel gobet <jmgobet@gmail.com>
2024-10-09 12:40:28 +0000

    Feature complete: Dictionary check of attempt

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,5 +1,6 @@
 import string
 import random
+import requests
 
 class Game:
     def __init__(self) -> list:
@@ -13,10 +14,19 @@ class Game:
         if not word:
             return False
 
+        '''Check if all letters in word are in the grid'''
         letters = self.grid.copy() # Consume letters from the grid
         for letter in word:
             if letter in letters:
                 letters.remove(letter)
             else:
                 return False
-        return True
+
+        '''Check if word is a real word in the dictionary'''
+        return self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://dictionary.lewagon.com/{word}")
+        json_response = response.json()
+        return json_response['found']
